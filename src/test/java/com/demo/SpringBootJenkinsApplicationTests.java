941d70e084bd5047dbed87290814d51d77e39bfa
Aymen Farhani <aymenfarhani28@gmail.com>
2025-07-25 12:58:07 +0200

    first commit

---
+++ src/test/java/com/demo/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("Spring Boot Test");
+    }
+
+}
